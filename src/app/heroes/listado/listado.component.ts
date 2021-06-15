f7121fa4973fbee916d1486657362771cc881be8
Christopher Kevin Espejo Arroyo <criskevin20@gmail.com>
2021-06-15 16:31:34 -0500

    primer commit seccion4 curso angular

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+
+})
+export class ListadoComponent {
+
+ heroes: string[]= ['Spidi', 'hulk', 'ironman', 'doctorstrange'];
+ heroeBorrado:string = '';
+ 
+ borrarHeroe(){
+  
+  
+  this.heroeBorrado= this.heroes.pop()||'';
+  
+
+   
+
+   
+   
+   
+ }
+
+}
